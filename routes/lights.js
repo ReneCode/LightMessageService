ed37181a1027b9b916a78b154c5034d0946a5426
rene_mbp <harryloveslearning@googlemail.com>
2017-03-04 16:10:25 +0100

    check data logging

--- routes/lights.js
+++ routes/lights.js
@@ -32,11 +32,11 @@ router.get('/', (req, res) => {
 });
 
 router.get('/latest', (req, res) => {
-	console.log('get latest');
 	Light.findOne({}).sort({date:-1}).exec( (err, data) => {
 		if (err) {
 			res.send(err);
 		} else {
+			console.log('get latest:', data);
 			res.send(data);
 		}		
 	})
